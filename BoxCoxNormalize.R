e80d9a5cf4934f41703a9a9870dfeb2fc5a83551
Junh0Jeon <damoeng77@gmail.com>
2023-05-30 17:44:36 +0900

    BoxCox Normalize with outlier

--- BoxCoxNormalize.R
+++ BoxCoxNormalize.R
@@ -56,16 +56,27 @@ CancerData[,notNormIndx + 2] %>% ggpairs()
 # CancerB에 대해 transformed value
 CancerB_BoxCox <- apply(CancerB[,totalRange], 2, function(x){
     forecast::BoxCox(x, "auto")
-})
+}) # Error
+# not positive라서 BoxCox가 진행되지 않음.
+# Mal에 대해서도, 사실 음수때문에 BoxCox를 loglik 추정법으로 얻은 lambda를 이용했을때
+# 적용이 안되는 case가 있었음. guerrero method로 하면 적용이 되었기에 무시했지만..
 
+## -> 음수 value 때문에 box-cox 제대로 적용이 안됨
+## -> 두가지 해결 방법 1. 최솟값을 적당히 더해서 양수화 2. Yeo-johnson transformation
+# 2번 선택.
+# 음수에 대해서도 적용 가능한 Yeo-Johnson transform을 적용해보려함.
+?car::yjPower()
+par(mfrow = c(1,2))
+temp <- boxCox(test~1, family = "yjPower")
+str(temp)
+temp$x[which.max(temp$y)]
+boxCox(test~1)
+?BoxCoxLambda()
+yj_trans(test)
 # CancerM에 대해 normal transform 한 다음 p.value
 CancerM_BoxCox_pval <- apply(CancerM_BoxCox, 2, function(x){
     shapiro.test(x)$p.value
 })
-## -> 음수 value 때문에 box-cox 제대로 적용이 안됨
-## -> 두가지 해결 방법 1. 최솟값을 적당히 더해서 양수화 2. Yeo-johnson transformation
-
-
 
 # 변환했는데도 normal이 아니라고 생각되는 경우를 확인
 CancerM_BoxCox_pval[CancerM_BoxCox_pval<0.05]
@@ -81,24 +92,4 @@ CancerData[,notNormIndx + 2] %>% ggpairs()
 
 
 
-## 함수 하나 제작
-# BoxCox로 univariate dataset을 Normalize 시키는 함수
-# guerrero랑 loglik중 더 p.value가 좋은 lambda estimate method로 적용함.
-# Parameter : data
-# return : normalized data
-# 사용할때 DescTools, nortest packages가 필요함.
-BoxCoxNormalize <- function(dat){
-    guerroBasedData <- BoxCox(dat, BoxCoxLambda(dat, "guerrero", lower = -2))
-    loglikBasedData <- BoxCox(dat, BoxCoxLambda(dat, "loglik", lower = -2))
-    
-    guerroPval <- shapiro.test(guerroBasedData)$p.value
-    loglikPval <- shapiro.test(loglikBasedData)$p.value
-    
-    if(loglikPval >= guerroPval){
-        return(list("value" = loglikBasedData, 
-                    "p.value" = loglikPval))
-    } else {
-        return(list("value" = guerroBasedData, 
-                    "p.value" = guerroPval))
-    }
-}
+# 함수 필요 없어짐.
