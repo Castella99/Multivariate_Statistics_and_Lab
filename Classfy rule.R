6fb3b060a87628162f8334270288b829d6a1c391
Junh0Jeon <damoeng77@gmail.com>
2023-06-02 13:27:00 +0900

    Classify Rule 업데이트 Normalize한거에는 변수 변환떄문에 Covariance Matrix가 너무 튀어서 그런지, 이게 return이 이상하게 나옴
    그래서 일단 normalize 하기 전의 data 기준으로, 수업의 Classification을 구현해보고 그걸 테스트해봄

--- Classfy rule.R
+++ Classfy rule.R
@@ -11,27 +11,27 @@ biotools::boxM(CancerDataRm_norm[,totalRange], CancerDataRm[,2])
 # 일치하지 않음.
 
 # 통계량 추정량 구하기
-
-
 # Mal group
-mu_M <- CancerDataRm_norm[CancerDataRm_norm$diagnosis=="M", totalRange] %>%
+mu_M <- CancerM_rm[,totalRange] %>%
     apply(., 2, mean)
-sigma_M <- CancerDataRm_norm[CancerDataRm_norm$diagnosis=="M", totalRange] %>%
+sigma_M <- CancerM_rm[,totalRange] %>%
     cov()
 # X_Mal ~ N(mu_M, sigma_M)을 따른다고 가정.
 
 # Ben group
-mu_B <- CancerDataRm_norm[CancerDataRm_norm$diagnosis=="B", totalRange] %>%
+mu_B <- CancerB_rm[,totalRange] %>%
     apply(., 2, mean)
-sigma_B <- CancerDataRm_norm[CancerDataRm_norm$diagnosis=="B", totalRange] %>%
+sigma_B <- CancerB_rm[,totalRange] %>%
     cov()
 # X_Ben ~ N(mu_B, sigma_B)를 따른다고 가정.
 # probability는 두가지 경우를 생각해볼 수 있음.
 # 1. 같은 경우
 # 2. 최초 dataset의 비율과 같음
 # 2번의 경우를 가정하겠음.
-1:3 %*% matrix(c(1,1,1,2,2,2,3,3,3), 3)
 
+p1 = nrow(CancerM_rm)/nrow(CancerDataRm)
+p2 = 1-p1
+log(p2/p1)
 ECM_rule <- function(x) {
     temp1 = log(base::det(sigma_M)/base::det(sigma_B))
     temp2 = (solve(sigma_M) - solve(sigma_B))
@@ -40,14 +40,16 @@ ECM_rule <- function(x) {
     result = -0.5*x%*%temp2%*%x + temp3 %*% x -0.5*temp4 -0.5*temp1
     return(result)
 }
-x <- rep(1,30)
-ECM_rule(testx)
-
-x <- unlist(CancerDataRm[200,totalRange])
+x <- unlist(CancerDataRm[10,totalRange])
+mean(x)
 
 ECM_rule(x)
-
-typeof(x)
-typeof(unlist(x))
- 
+# normalize 전에 test
+Rule1 <- apply(CancerDataRm[,totalRange], 1, function(x){
+    input <- unlist(x)
+    ifelse(ECM_rule(x)>=log(p2/p1), "M", "B")
+})
+Rule1 <- as.factor(Rule1)
+mean(Rule1 == CancerDataRm$diagnosis)
+# 전체 기준 97% 정답률
 
